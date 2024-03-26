2771333b3af3b84878e450c2ff4665fb65f689ce
neenavarughese55 <158041703+neenavarughese55@users.noreply.github.com>
2024-03-26 22:12:17 +0000

    Added the message to show the user has left the chat.

--- public/js/client.js
+++ public/js/client.js
@@ -52,15 +52,29 @@ socket.on("new user", function (data) {
       });
 });
 
-//when a user leaves
-socket.on("user disconnected", function (userName) {
-  document.querySelector(`.${userName}-userlist`).remove();
-});
+
 
 
 const inputField = document.querySelector(".message_form__input");
 const messageForm = document.querySelector(".message_form");
 const messageBox = document.querySelector(".messages__history");
+
+//when a user leaves
+socket.on("user disconnected", function (userName) {
+  document.querySelector(`.${userName}-userlist`).remove();
+  const time = new Date();
+  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+  const receivedMsg = `
+  <div class="incoming__message">
+    <div class="received__message">
+      <p>${userName} has left</p>
+      <div class="message__info">
+        <span class="time_date">${formattedTime}</span>
+      </div>
+    </div>
+  </div>`;
+  messageBox.innerHTML += receivedMsg;
+});
 
 const addNewMessage = ({ user, message }) => {
   const time = new Date();
